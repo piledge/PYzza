19738042df706cc02ab3d4942ce87b99f756e328
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-12-09 19:05:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,15 +18,15 @@ setup(
     author = 'pi-ledge',
     author_email = '',
     url = 'https://github.com/pi-ledge/pyzza/',
-    description = 'A simple example python package.',
-    long_description_content_type = "text/x-rst",  # If this causes a warning, upgrade your setuptools package
+    description = 'Small but useful collection of Python-Functions',
+    long_description_content_type = "text/x-rst",
     long_description = long_description,
     license = "MIT license",
-    packages = find_packages(exclude=["test"]),  # Don't include test directory in binary distribution
+    packages = find_packages(exclude=["test"]),
     install_requires = requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]  # Update these accordingly
+    ]
 )
