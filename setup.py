8ced462236e240efeaf14ecbb97b2eb271369172
pi-ledge <michael.lechner@icloud.com>
2024-11-07 09:49:55 +0100

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements(file):
 def read_file(file):
    with open(file) as f:
         return f.read()
-    
+
 long_description = read_file("README.md")
 version = read_file("VERSION")
 requirements = read_requirements("requirements.txt")
@@ -19,7 +19,7 @@ setup(
     author_email = '',
     url = 'https://github.com/piledge/pyzza/',
     description = 'Small but useful collection of Python-Functions',
-    long_description_content_type = "text/x-rst",
+    long_description_content_type = "Small but useful collection of Python-Functions",
     long_description = long_description,
     license = "MIT license",
     packages = find_packages(exclude=["test"]),
