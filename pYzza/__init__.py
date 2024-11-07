51df13753586c6a99ae571740aac8de0d353e9e0
pi-ledge <michael.lechner@icloud.com>
2024-11-07 12:40:52 +0100

    Header

--- pYzza/__init__.py
+++ pYzza/__init__.py
@@ -1,1 +1,13 @@
+"""
+Copyright 2023 [piledge]. Licensed under the MIT license.
+
+pYzza is a small collection of Python-Functions created by a Pizza-enthusiast and with no specific topic.
+
+This package is a result of me constantly breaking the DRY principle by copy-and-pasting stuff from old projects into new ones and to have a centralized database to access the functions from several computing instances.
+
+The pYzza-package does not solve any large problem, but it can help to save time by eliminating repetitive code and provide easy access to some more complicated interfaces of different packages.
+
+This package is constantly being expanded and improved. Any ideas for new functions or bug reports will be appreciated.
+"""
+
 from .pYzza import *
